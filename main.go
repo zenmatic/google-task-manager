d617b0a87e93a7e5e6544ae6498fd7d1ac4f7d97
Matt Dunford <zenmatic@gmail.com>
2020-11-07 11:12:56 -0800

    listAllTasklists() and "list tasks in listname"

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
         "log"
         "net/http"
         "os"
+        "strings"
 
         "golang.org/x/net/context"
         "golang.org/x/oauth2"
@@ -76,6 +77,20 @@ func saveToken(path string, token *oauth2.Token) {
         json.NewEncoder(f).Encode(token)
 }
 
+func listAllTasklists(srv *tasks.Service) error {
+        r, err := srv.Tasklists.List().MaxResults(TasklistsMaxResults).Do()
+        if err != nil {
+                return err
+        }
+        fmt.Println("Task Lists:")
+        if len(r.Items) > 0 {
+                for _, i := range r.Items {
+                        fmt.Printf("- %v\n", i.Title)
+                }
+        }
+        return nil
+}
+
 func getIdFromName(srv *tasks.Service, name string) (string, error) {
 
         r, err := srv.Tasklists.List().MaxResults(TasklistsMaxResults).Do()
@@ -83,10 +98,9 @@ func getIdFromName(srv *tasks.Service, name string) (string, error) {
                 return "", err
         }
 
-        fmt.Println("Task Lists:")
         if len(r.Items) > 0 {
                 for _, i := range r.Items {
-                        fmt.Printf("%s (%s)\n", i.Title, i.Id)
+                        //fmt.Printf("%s (%s)\n", i.Title, i.Id)
                         if i.Title == name {
                                 return i.Id, nil
                         }
@@ -96,17 +110,17 @@ func getIdFromName(srv *tasks.Service, name string) (string, error) {
         return "", errors.New("no matching list")
 }
 
-func listTasksInTasklist(srv *tasks.Service, tasklistId string) error {
+func listTasksInTasklist(srv *tasks.Service, tasklistId string, tasklistName string) error {
 
         r, err := srv.Tasks.List(tasklistId).MaxResults(TasksMaxResults).Do()
         if err != nil {
                 return err
         }
 
-        fmt.Printf("Tasks in %v Lists:", tasklistId)
+        fmt.Printf("Tasks in %v:\n", tasklistName)
         if len(r.Items) > 0 {
                 for _, i := range r.Items {
-                        fmt.Printf("%s (%s)\n", i.Title, i.Id)
+                        fmt.Printf("- %s\n", i.Title)
                 }
         }
 
@@ -114,6 +128,7 @@ func listTasksInTasklist(srv *tasks.Service, tasklistId string) error {
 }
 
 func main() {
+
         b, err := ioutil.ReadFile("credentials.json")
         if err != nil {
                 log.Fatalf("Unable to read client secret file: %v", err)
@@ -122,8 +137,7 @@ func main() {
         // If modifying these scopes, delete your previously saved token.json.
         config, err := google.ConfigFromJSON(b, tasks.TasksReadonlyScope)
         if err != nil {
-                log.Fatalf("Unable to parse client secret file to config: %v", err)
-        }
+                log.Fatalf("Unable to parse client secret file to config: %v", err) }
         client := getClient(config)
 
         srv, err := tasks.New(client)
@@ -131,11 +145,42 @@ func main() {
                 log.Fatalf("Unable to retrieve tasks Client %v", err)
         }
 
-        lname := "A list"
-        id, err := getIdFromName(srv, lname)
-        if err != nil {
-                log.Fatalf("Unable to find task list '%v': %v", lname, err)
+        args := os.Args[1:]
+        cmd := strings.Join(args, " ")
+
+        if cmd == "list tasks" {
+                // list tasks from default tasklist
+                lname := DefaultListName
+                id, err := getIdFromName(srv, lname)
+                if err != nil {
+                        log.Fatalf("Unable to find task list '%v': %v", lname, err)
+                }
+                listTasksInTasklist(srv, id, lname)
+        } else if cmd == "list all tasks" {
+                // list all from all task lists
+        } else if cmd == "list tasklists" {
+                err := listAllTasklists(srv)
+                if err != nil {
+                        log.Fatalf("Unable to list task lists: %v", err)
+                }
+        } else if len(args) > 3 && strings.HasPrefix(cmd, "list tasks in ") {
+                // TODO list tasks in "tasklist name"
+                tasklistName := strings.Join(args[3:], " ")
+                id, err := getIdFromName(srv, tasklistName)
+                if err != nil {
+                        log.Fatalf("Unable to find task list '%v': %v", tasklistName, err)
+                }
+                listTasksInTasklist(srv, id, tasklistName)
         }
-        fmt.Printf("found %v for '%v'\n", id, lname)
-        listTasksInTasklist(srv, id)
+        /*
+        } else {
+                n, err := fmt.Sscanf(cmd, `move tasks from "%s" to "%s"`, &srcTasklist, &dstTasklist)
+                if (err != nil) {
+                        log.Fatalf("Unable to parse arguments: %v", cmd)
+                }
+                if (n == 2) {
+                        // move tasks
+                }
+        }
+        */
 }
